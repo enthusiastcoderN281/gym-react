99247d97793837703e53de2173bd25266bc2e9a1
Sahil Jain <sahilmjain03@gmail.com>
2022-09-02 22:23:31 +0530

    navbar rendered, tried bgImage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import MyNavBar from './components/MyNavBar.js';
+
+//importing bgImage component
+// import BgImage from './components/BgImage.js';
+
+
+//example of bootstrap
+// import { Button } from 'react-bootstrap';
+
+// function CreateButton(){
+//   return (
+//     <Button variant="primary">Primary</Button>
+//     // <h2> hellow world!!!!</h2>
+//   );
+// }
+
 
 function App() {
   return (
     <div className="App">
-      
+      <MyNavBar />
     </div>
   );
 }
---
+++ src/components/BgImage.js
@@ -0,0 +1,14 @@
+// import bgimage from '/src/dragon_fruit.jpg';
+import 'D:\\Sahil\\coding\\gymReact\\src\\bgImage.css';
+
+
+function BgImage()
+{
+    return(
+        <img class = "image" src='/src/dragon_fruit.jpg'></img>
+    );
+}
+
+
+export default BgImage;
+

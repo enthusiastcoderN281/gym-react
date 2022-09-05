9f04b4008bcba9bf503a28e540f6ee7d5280c27f
Sahil Jain <sahilmjain03@gmail.com>
2022-09-05 21:35:41 +0530

    test commit js

--- src/components/Text.js
+++ src/components/Text.js
@@ -7,5 +7,5 @@ function Text(){
         <div className='text-light bg-dark child1'><p>THE BEST FITNESS CLUB IN TOWN</p></div>
         <div className='child2 '><span>SHAPE</span> YOUR IDEAL BODY</div>
     </div> );
-    }
+    } 
 export default Text;

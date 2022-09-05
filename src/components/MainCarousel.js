0d9b74ab5b042a1677c7d5515203431cb7efd1d7
Sahil Jain <sahilmjain03@gmail.com>
2022-09-05 23:56:54 +0530

    carousel

---
+++ src/components/MainCarousel.js
@@ -0,0 +1,97 @@
+import Carousel from 'react-bootstrap/Carousel';
+import car1 from '../car1.png'
+import car2 from '../car2.png'
+import car3 from '../car3.png'
+
+function MainCarousel() {
+  return (
+    <>
+    <Carousel className='MainCarousel'>
+      <Carousel.Item className='w-100'>
+        <img
+          className="d-block w-100"
+          src={car1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={car2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={car3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+
+{/* mini carousel */}
+
+<Carousel className='miniCarousel'>
+      <Carousel.Item className="w-33">
+        <img
+          className="d-block w-33"
+          src={car1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item className="w-33">
+        <img
+          className="d-block w-33"
+          src={car2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item className="w-33">
+        <img
+          className="d-block w-33"
+          src={car3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+
+
+    </>);
+}
+
+export default MainCarousel;
